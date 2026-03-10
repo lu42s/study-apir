dfedd496e1d535663b5d91571f0df213c1966b48
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 22:31:40 -0300

    aula 0903

--- src/main/java/br/com/fiap/study_apir/controller/PingController.java
+++ src/main/java/br/com/fiap/study_apir/controller/PingController.java
@@ -1,16 +1,16 @@
-package br.com.fiap.study_apir;
+package br.com.fiap.study_apir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("raiz")
+@RequestMapping
 public class PingController {
 
     @GetMapping("ping")
     public String ping(){
-        return "pong";
+        return "pong teste";
     }
 
     
