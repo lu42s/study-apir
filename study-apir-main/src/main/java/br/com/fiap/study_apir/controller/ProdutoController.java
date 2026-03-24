378480ad200ab1589d0b3041138a5187416b0587
lu <luisa.souzasantos42@gmail.com>
2026-03-23 22:44:30 -0300

    atualização-2303

---
+++ study-apir-main/src/main/java/br/com/fiap/study_apir/controller/ProdutoController.java
@@ -0,0 +1,43 @@
+package br.com.fiap.study_apir.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.study_apir.model.Produto;
+import br.com.fiap.study_apir.repository.RepositoryProdutoMockup;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private RepositoryProdutoMockup mockup = new RepositoryProdutoMockup();
+
+    @PostMapping
+    public ResponseEntity<String> create(){
+        return ResponseEntity.status(HttpStatus.CREATED).body("Produto criado");
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(Long id){
+        Produto produto = mockup.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(produto);
+    }
+
+    @PutMapping
+        public ResponseEntity<String> update(){
+        return ResponseEntity.status(HttpStatus.OK).body("Produto atualizado");
+    }
+
+    @DeleteMapping
+        public ResponseEntity<String> delete(){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Produto excluido");
+    }
+
+}
+
